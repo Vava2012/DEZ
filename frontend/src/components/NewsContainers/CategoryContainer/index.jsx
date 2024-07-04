32c01cc10f8160a779097205a9dd4dd262a40bf4
Vava2012 <167721016+Vava2012@users.noreply.github.com>
2024-07-04 17:39:22 +0200

    test

--- frontend/src/components/NewsContainers/CategoryContainer/index.jsx
+++ frontend/src/components/NewsContainers/CategoryContainer/index.jsx
@@ -25,7 +25,7 @@ const NewsContainer = () => {
                     </Paragraph> 
                     </TextSection>
                     <ImageSection>
-                    <img src="/testslike/115x86.webp" alt="NHL" />
+                    <img src="/testslike/roko simic 580x393.webp" alt="NHL" />
                     </ImageSection>
                 </MainTop>
 
--- frontend/src/routes/HelpForForeigners/index.jsx
+++ frontend/src/routes/HelpForForeigners/index.jsx
@@ -2,12 +2,13 @@ import CardNews from "../../components/NewsContainers/CardNews/index.jsx";
 import BottomSlider from "../../components/NewsContainers/Slider/index.jsx";
 
 
-
+//ovo je za test
 
 const HelpForForeigners = () => {
   return (
     <div>
-      <CardNews />
+      
+      <BottomSlider />
       
       
     </div>
