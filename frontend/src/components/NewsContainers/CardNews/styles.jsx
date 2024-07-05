16a47bf138719c94c797b7122d77314d76458cfd
Toni Pinjuh <tonipinjuh89@gmail.com>
2024-07-05 14:27:37 +0200

    cardnesto

--- frontend/src/components/NewsContainers/CardNews/styles.jsx
+++ frontend/src/components/NewsContainers/CardNews/styles.jsx
@@ -1,31 +1,43 @@
 import styled from "styled-components";
 
+export const BorderContainer = styled.div`
+  padding: 10px 10px 0px 0px;
+  border-top: 1px solid #d0d0d0;
+  border-bottom: 1px solid #d0d0d0;
+ 
+ 
+  
+`;
+
 export const NewsContainer = styled.div`
   display: flex;
   flex-wrap: wrap;
   justify-content: space-between;
   align-items: flex-start; /* Align to the left */
-  background-color: #d62222;
+  background-color: #ffffff;
   max-width: 1200px;
   width: 74%;
   padding: 20px;
-  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+  border-right: 1px solid #d0d0d0;
+ 
   margin-bottom: 20px;
-  
+
   @media (max-width: 768px) {
     width: 90%;
     height: 90%;
   }
-
 `;
 
 export const NewsItem = styled.div`
   width: calc(33.333% - 10px);
   margin-bottom: 20px;
   cursor: pointer;
-  transition: transform 0.5s ease-in-out;   
+  transition: transform 0.5s ease-in-out;
+  border-right: 1px solid #d0d0d0;
+  border-top: 1px solid #d0d0d0;
+  margin: 0 auto; 
   
- 
+  
 `;
 
 export const NewsTitle = styled.h2`
